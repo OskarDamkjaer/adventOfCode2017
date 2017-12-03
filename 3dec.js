7725e62841fbc0ba346d5e834fa2edbf7e508cab
Oskar Damkjaer <oskar.damkjaer@gmail.com>
2017-12-03 12:04:10 +0100

    day three

---
+++ 3dec.js
@@ -0,0 +1,6 @@
+// input = 289326
+
+// Part one. Each size is square  (1 + 2n)^2, sqrt(input) yields 537.decimals meaning it is on square with width 539, with an inner square width 537. input - 537^2 = how far to go along.
+
+
+// Part two. Do lookup in https://oeis.org/A141481
