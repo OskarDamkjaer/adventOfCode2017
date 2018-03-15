fbebcf0eca7298ecc33e8de1928caa572dd16435
Oskar Damkjaer <oskar.damkjaer@gmail.com>
2018-03-15 15:38:00 +0100

    part1 done

--- 11/solve.js
+++ 11/solve.js
@@ -1,24 +1,51 @@
-const fs = require('fs')
-const input = fs.readFileSync('./input.txt', 'utf-8')
+const fs = require('fs');
+const input = fs.readFileSync('./input.txt', 'utf-8');
 
-const instructions = input
-  .trim()
-  .split(',')
+const partOne = function(arg) {
+  const path  = arg.trim().split(',');
+  const mymap = new Map();
+  mymap.set('nw', 0);
+  mymap.set('n', 0);
+  mymap.set('ne', 0);
+  mymap.set('sw', 0);
+  mymap.set('s', 0);
+  mymap.set('se', 0);
 
-const partOne = function (path) {
-  const mymap = new Map()
-  mymap.set("nw", 0)
-  mymap.set("n", 0)
-  mymap.set("ne", 0)
-  mymap.set("sw", 0)
-  mymap.set("s", 0)
-  mymap.set("se", 0)
-
-  for(let i = 0; i < path.length; i++){
-    mymap.set(path[i], parseInt(mymap.get(path[i])) + 1)
+  for (let i = 0; i < path.length; i++) {
+    mymap.set(path[i], parseInt(mymap.get(path[i])) + 1);
   }
 
-  console.log(mymap)
-}
+  const cancelOut = (key, oppositeKey) => {
+    let first = mymap.get(key);
+    let second = mymap.get(oppositeKey);
+    if (first > second) {
+      mymap.set(key, first - second);
+      mymap.set(oppositeKey, 0);
+    } else {
+      mymap.set(oppositeKey, second - first);
+      mymap.set(key, 0);
+    }
+  };
 
-partOne(instructions)
+  cancelOut('n', 's');
+  cancelOut('ne', 'sw');
+  cancelOut('nw', 'se');
+
+  const sideCancel = (key, oppositeKey) => {
+    let first = mymap.get(key);
+    let second = mymap.get(oppositeKey);
+    if (first > second) {
+      mymap.set(oppositeKey, 0);
+    } else {
+      mymap.set(key, 0);
+    }
+  };
+
+  sideCancel('nw', 'ne');
+  sideCancel('sw', 'se');
+  return Array.from(mymap.values()).reduce((a, b) => a + b, 0);
+};
+
+const partTwo = () => {}
+
+module.exports = {partOne, partTwo};
--- 11/test.js
+++ 11/test.js
@@ -1,17 +1,17 @@
-const assert = require('assert')
-const solve = require('./solve')
+const assert = require('assert');
+const solve = require('./solve');
 
-describe('partTwo(inputString)', function () {
-  it('should return 3', function () {
-    assert.equal(solve.partTwo("ne,ne,ne"), 3)
-  })
-  it('should return 0', function () {
-    assert.equal(solve.partOne("ne,ne,sw,sw"), 0)
-  })
-  it('should return 2', function () {
-    assert.equal(solve.partOne("se,se"), 2)
-  })
-  it('should return 3 on s,s,sw', function () {
-    assert.equal(solve.partOne("s,s,sw"), 3)
-  })
-})
+describe('partOne(inputString)', function() {
+  it('should return 3', function() {
+    assert.equal(solve.partTwo('ne,ne,ne'), 3);
+  });
+  it('should return 0', function() {
+    assert.equal(solve.partOne('ne,ne,sw,sw'), 0);
+  });
+  it('should return 2', function() {
+    assert.equal(solve.partOne('se,se'), 2);
+  });
+  it('should return 3 on s,s,sw', function() {
+    assert.equal(solve.partOne('s,s,sw'), 3);
+  });
+});
